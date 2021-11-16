63827c9ce6578ee5ff67c255b7a4839d1b55b521
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2021-11-16 18:38:58 +0100

    finished

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-let age = prompt("what's your age ?")
-let travelingDistance = prompt("how many km are you traveling ?")
+
+let age = Number(prompt("what's your age ?"))
+
+let travelingDistance = Number(prompt("how many km are you traveling ?"))
 const pricePerKm = 0.21 
 
 let travelPrice = 0.21 * travelingDistance
 
-if(age > 18 && age < 65){
-  travelPrice = alert(` Ticket price will be : ${0.21 * travelingDistance}£`) 
+let discountPrice
+
+if( typeof(age) !== "number" || typeof(travelingDistance) !== "number"){
+    console.log("You are entering a wrong data type")
 }
 
-if(age < 18){
-    let discountPrice = alert(`Normal ticet price would be ${travelPrice}£  but you are getting 20% of ${0.21 * travelingDistance * 0.20}£ and the new price will be  ${travelPrice - 0.21 * travelingDistance * 0.20}£`)
+else if(age > 18 && age < 65){
+  travelPrice = alert(` Ticket price will be : ${pricePerKm * travelingDistance}£`) 
+} 
+
+else if(age < 18){
+    discountPrice = alert(`Normal ticet price would be ${travelPrice}£  but you are getting 20% of
+    ${pricePerKm * travelingDistance * 0.20(2)}£ and the new price will be  ${travelPrice - pricePerKm * travelingDistance * 0.20}£`)
 }
 
-if(age >= 65){
-    discountPrice = alert(`Normal ticet price would be ${travelPrice}£  but you are getting 40% of ${0.21 * travelingDistance * 0.40}£ and the new price will be  ${travelPrice - 0.21 * travelingDistance * 0.40}£ `)
+else if(age >= 65){
+    discountPrice = alert(`Normal ticet price would be ${travelPrice}£  but you are getting 40% of
+    ${pricePerKm * travelingDistance * 0.40}£ and the new price will be  ${travelPrice - pricePerKm * travelingDistance * 0.40}£`)
 }
